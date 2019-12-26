ac91130cafde749fbd9368d334aa976f77c6e55f
sarah wang <sarahwang47@gmail.com>
2019-12-26 16:44:47 +0800

    redux added

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,20 @@
 // external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import citiesReducer from './reducers/citiesReducer';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
-
-// State and reducers
-const reducers = combineReducers({
-  cities: citiesReducer
-});
 
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.querySelector('.container')
 );
